b43f2b0d1a13310072f0a611ecfa73f8f4ee82ee
Mingyu Lin <mylin651@gmail.com>
2017-10-15 04:06:30 -0700

    set the logic in sessions controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Boomerang - Real-time social networking app"
     if page_title.empty?
         base_title
     else
