391bec290013cd030d056dd73cfdc05fd47e570b
Mingyu Lin <mylin651@gmail.com>
2017-10-15 00:01:11 -0700

    Modified css file, and add new controller for sign-in and sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'static_pages/interest'
 
   root to: 'visitors#index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', passwords: 'users/passwords' }
   resources :users
 end
