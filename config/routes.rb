b48ee2a82aca8aca099acd0c42983a787a18582c
Mingyu Lin <mylin651@gmail.com>
2017-10-15 15:20:27 -0700

    original version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
    # get 'matches/listing'
 
-  root to: 'visitors#index'
+  root to: 'visitors#mood'
   get 'mood', to: 'visitors#mood'
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', passwords: 'users/passwords' }
   resources :users
