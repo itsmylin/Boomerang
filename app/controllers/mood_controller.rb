80edebd5f0f5540d565ab88ae87859bfdc07e061
Mingyu Lin <mylin651@gmail.com>
2017-10-18 19:44:57 -0700

    one user one interestid non-duplicated

--- app/controllers/mood_controller.rb
+++ app/controllers/mood_controller.rb
@@ -1,4 +1,5 @@
 class MoodController < ApplicationController
+  require 'date'  
   def index
     @interestList=Interest.all
   end
@@ -10,10 +11,19 @@ class MoodController < ApplicationController
       @interest_id = params[:interest_id]
       #map = { userID: @user_id, interestID:@interest_id }
       #puts map
-      UserInterestMapping.create( userID: @user_id, interestID:@interest_id )
+      @update_user = UserInterestMapping.where(userID: @user_id).first()
+      if @update_user == nil
+        UserInterestMapping.create( userID: @user_id, interestID:@interest_id )
+      else
+        if @update_user.interestID == @interest_id.to_s
+          @update_user.save 
+        elsif @update_user.interestID != @interest_id.to_s
+          @update_user.interestID = @interest_id
+          @update_user.save
+        end
+      end 
     end
     redirect_to match_index_path
   end
-    
   
 end
