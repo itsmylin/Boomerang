f10005bd1dcac49842d5b5e83a5afae52252f5df
danielmeans <dameans@gmail.com>
2017-10-18 19:37:30 -0700

    Fixed Refresh problem on Iinterests

--- app/controllers/mood_controller.rb
+++ app/controllers/mood_controller.rb
@@ -1,6 +1,26 @@
 class MoodController < ApplicationController
   def index
     @interestList=Interest.all
+    
+# Function to convert interest list into chunks of 3 for display
+    @interestChunks =  []
+    @counter = 0
+    @helperArr =[]
+    @interestList.each do  |interest|
+      if @counter == 2
+        @helperArr << interest
+        @interestChunks << @helperArr
+        @helperArr = []
+        @counter = 0
+      else
+        @helperArr << interest
+        @counter += 1
+      end 
+    end
+   puts @interestChunks     
+
+
+
   end
   def create
     session[:interest_id] = params[:interest_id]
