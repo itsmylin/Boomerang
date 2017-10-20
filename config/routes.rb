98d1b8448ac329553ea761f4fffc08491f11a739
abbasinasb <a.abbasinasab@gmail.com>
2017-10-20 16:16:30 -0700

    fixed functions show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'mood#index'
   devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', passwords: 'users/passwords' }
   resources :users do
-    get 'notify', :on => :collection
+    post 'notify', :on => :collection
   end
   resources :mood
   resources :match
