3d0159153c233a388be2f4f181e498cbea44a902
Mingyu Lin <mylin651@gmail.com>
2017-10-25 16:15:16 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20171014175639) do
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
     end
+    create_table "user_user_mappings", force: :cascade do |t|
+      t.string   "primeUserID", null: false
+      t.string   "secUserID",   null: false
+      t.string   "typeName"
+      t.string   "status"
+      t.string   "timeslot"
+      t.datetime "created_at",  null: false
+      t.datetime "updated_at",  null: false
+    end
   
     create_table "users", force: :cascade do |t|
       t.string   "email",                  default: "", null: false
