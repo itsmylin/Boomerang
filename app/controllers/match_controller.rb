bf74b37ff40a2092acc5ba6c6be75d8059db2410
Mingyu Lin <mylin651@gmail.com>
2017-10-28 17:25:25 -0700

    match controller

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -1,34 +1,54 @@
 class MatchController < ApplicationController
   def index
-
-
-    if session[:interest_id] != nil
-      @userInterestMappingList = UserInterestMapping.where(interestID: session[:interest_id])
+    set_interestID
+    if session[:interest_id] != nil && user_signed_in?
+      @userInterestMapping = UserInterestMapping.where(interestID: session[:interest_id]).where.not(userID: current_user[:id]).sample()
+    elsif session[:interest_id] != nil && !user_signed_in?
+      @userInterestMapping = UserInterestMapping.where(interestID: session[:interest_id]).sample()
     else
-      @userInterestMappingList = UserInterestMapping.all
-      puts "else"
+      @userInterestMapping = UserInterestMapping.all.sample()
     end
     #@userInterestMappingList = User.all
-    @users = []
+
     # @userInterestMappingList.each do |map|
     #   @maps <<  { id: User.find(map.userID) , interest: Interest.find(map.interestID) }
     # end
-    @userInterestMappingList.each do |map|
-      if user_signed_in?
-        unless  map.userID.to_i == current_user.id
-         @users <<  User.find(map.userID)
-        end
-      else
-        @users <<  User.find(map.userID)
+    # @userInterestMappingList.each do |map|
+    #   if user_signed_in?
+    #     unless map.userID.to_i == current_user.id
+    #       @users <<  User.find(map.userID)
+    #     end
+    #   else
+    #     @users <<  User.find(map.userID)
+    #   end
+    # end
+    if @userInterestMapping != nil
+      @user = User.find(@userInterestMapping[:userID])
+    else
+      @user = nil
+    end
+  end
+  def create
+    if !user_signed_in?
+      redirect_to new_user_session_path
+    elsif current_interestID == nil
+      redirect_to mood_index_path
+    else
+      # update database
+      @userInterestMappingList = UserInterestMapping.where(interestID: session[:interest_id]).where.not(userID: current_user[:id]).sample()
+      @user = User.find(@userInterestMapping[:userID])
+      respond_to do |format|
+        format.js
       end
     end
-
-
-    puts @users
-    #puts @userInterestMappingList[0][:id]
-    #puts @userInterestMappingList1[0][:userID]
   end
-
   def show
+  end
+  
+  protected
+  def set_interestID
+    if user_signed_in? 
+      session[:interest_id] = current_interestID
+    end
   end
 end
