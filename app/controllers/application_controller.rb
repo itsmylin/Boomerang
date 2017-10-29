e4e4959c6bb885dafcdd458bd4f7ff0decbb70d0
Mingyu Lin <mylin651@gmail.com>
2017-10-28 17:50:28 -0700

    match pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,18 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
+  private
+  # function for define current_interestID for the everytime loading webpage
+  def current_interestID
+    if current_user == nil 
+      @current_interestID = nil 
+    elsif UserInterestMapping.where(userID: current_user[:id]) == []
+      @current_interestID = nil
+    else
+      @current_interestID = UserInterestMapping.where(userID: current_user[:id])[0][:interestID]
+    end
+    @current_interestID
+  end
   def configure_permitted_parameters
     #devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     #devise_parameter_sanitizer.permit(:account_update, keys: [:name])
