c2cf7b82f03142677de89a94ea871c75e45502e1
Ishani Gupta <ishaniGupta@169-231-163-190.wireless.ucsb.edu>
2017-11-01 14:27:53 -0700

    Inbox Changes

--- app/controllers/match_controller.rb
+++ app/controllers/match_controller.rb
@@ -30,60 +30,104 @@ class MatchController < ApplicationController
 	end
 
     def updateResponse()
+        puts 'I am inside update Response'
         @fromUser=params[:primuser];
         @toUser=params[:secuser];
         @response=params[ :response];
         @presUserData=UserUserMapping.find_by primeUserID: @fromUser
         @secUserData=UserUserMapping.find_by primeUserID: @toUser
 
+        puts @presUserData.inspect
+        puts @secUserData.inspect
+        
+        #puts 'I am printing all the incoeming values'
+        #puts @fromUser
+        #puts @toUser
+        #puts @response
+
         if(@response=='Y')
         #prim user is the logged in user whereas the sec user is the one abt whom the prim user is giving prefernce(yes/no)
-        @secUserReject=(secUserData["nomatch"]).split(',')
-        puts 'list of users rejected by sec User '+ @secUserReject
-        @secUserLiked=(secUserData["sent"]).split(',')
-        puts 'list of users liked by sec user '+ @secUserLiked
-        if(@secUserReject.include? @fromUser) #Already No from secUSer
-            @presUserNoMatchDataArr=(presUserData["nomatch"]).split(',')
-            @presUserNoMatchDataArr=@presUserNoMatchDataArr + @toUser #there is not diff btw who said no first ?? shu we handle this case too later?
-            @presUserNoMatchData=@presUserNoMatchDataArr.join(',')
-            puts 'presUserNoMatchData'+ @presUserNoMatchData
-            UserUserMapping.where( primeUserID: @fromUser ).update_all( nomatch: @presUserNoMatchData )
+        puts 'Inside response is Yes'
+            if(@secUserData==nil)
+                puts 'secUserData is nill'
+            end
+
+        
+        @secUserReject=(@secUserData["nomatch"]).split(',')
+       # puts 'list of users rejected by sec User '+ @secUserReject
+        @secUserLiked=(@secUserData["sent"]).split(',')
+       # puts 'list of users liked by sec user '+ @secUserLiked
+            if(@secUserReject.include? @fromUser) #Already No from secUSer
+                @presUserNoMatchDataArr=(@presUserData["nomatch"]).split(',')
+                @presUserNoMatchDataArr=@presUserNoMatchDataArr.push(@toUser) #there is not diff btw who said no first ?? shu we handle this case too later?
+                @presUserNoMatchData=@presUserNoMatchDataArr.join(',')
+                puts 'presUserNoMatchData'+ @presUserNoMatchData
+                UserUserMapping.where( primeUserID: @fromUser ).update_all( nomatch: @presUserNoMatchData )
+            
+
+            elsif(@secUserLiked.include? @fromUser) #Already Yes from secUser
+                    #remove 
+                    #
+                    @presUserPrfctMatchDataArr=(@presUserData["completematch"]).split(',')
+                    @presUserPrfctMatchDataArr=@presUserPrfctMatchDataArr.push(@toUser)
+                    @presUserPrfctMatchData=@presUserPrfctMatchDataArr.join(',')
+                    puts 'presUserPrfctMatchData'+ @presUserPrfctMatchData
+                    UserUserMapping.where( primeUserID: @fromUser ).update_all( completematch: @presUserPrfctMatchData )
+
+                    @secUserPrfctMatchDataArr=(@secUserData["completematch"]).split(',')
+                    @secUserPrfctMatchDataArr=@secUserPrfctMatchDataArr.push(@fromUser)
+                    @secUserPrfctMatchData=@secUserPrfctMatchDataArr.join(',')
+                    puts 'secUserPrfctMatchData'+ @secUserPrfctMatchData
+                    UserUserMapping.where( primeUserID: @toUser ).update_all( completematch: @secUserPrfctMatchData )
 
 
-        elsif(@secUserLiked.include? @fromUser) #Already Yes from secUser
-                #remove
-                #
-                @presUserPrfctMatchDataArr=(presUserData["completematch"]).split(',')
-                @presUserPrfctMatchDataArr=@presUserPrfctMatchDataArr+@toUser
-                @presUserPrfctMatchData=@presUserPrfctMatchDataArr.join(',')
-                 puts 'presUserPrfctMatchData'+ @presUserPrfctMatchData
-                UserUserMapping.where( primeUserID: @fromUser ).update_all( completematch: @presUserPrfctMatchData )
-
-                @secUserPrfctMatchDataArr=(secUserData["completematch"]).split(',')
-                @secUserPrfctMatchDataArr=@secUserPrfctMatchDataArr+','+@fromUser
-                @secUserPrfctMatchData=@secUserPrfctMatchDataArr.join(',')
-                puts 'secUserPrfctMatchData'+ @secUserPrfctMatchData
-                UserUserMapping.where( primeUserID: @toUser ).update_all( completematch: @secUserPrfctMatchData )
 
 
+            else
+                
+                @presUserSentArr=(@presUserData["sent"]).split(',')
+                @presUserSentArr=@presUserSentArr.push(@toUser)
+                @presUserSent=@presUserSentArr.join(',')
+                UserUserMapping.where( primeUserID: @fromUser ).update_all( sent: @presUserSent )
+
+        
+                @secUserRcvArr=(@secUserData["received"]).split(',')
+                @secUserRcvArr=@secUserRcvArr.push(@fromUser)
+                @secUserRcv=@secUserRcvArr.join(',')
+                UserUserMapping.where( primeUserID: @toUser ).update_all( received: @secUserRcv )
+            end
+
+        elsif(@response == 'N')
+            #they both are mismatch !
+            #updating the present user no match
+            @primUserNoMatchDataArr= (@presUserData["nomatch"]).split(',')
+            @primUserNoMatchDataArr= @primUserNoMatchDataArr.push(@toUser)
+            @primUserNoMatchData=@primUserNoMatchDataArr.join(',')
+            UserUserMapping.where( primeUserID: @fromUser ).update_all(nomatch: @primUserNoMatchData)
+            
+            #updating the secondary user no match
+            @secUserNoMatchDataArr= (@secUserData["nomatch"]).split(',')
+            @secUserNoMatchDataArr= @secUserNoMatchDataArr.push(@fromUser)
+            @secUserNoMatchData=@secUserNoMatchDataArr.join(',')
+            UserUserMapping.where( primeUserID: @toUser ).update_all(nomatch: @secUserNoMatchData)
+
+            @secUserLikedArr=(@secUserData["sent"].split(','))
+            if(@secUserLikedArr.include? @fromUser)
+                #removing from sec liked as prim has said no
+                
+                @secUserLikedArr=@secUserLikedArr-[@fromUser]
+                @secUserLikedData=@secUserLikedArr.join(',')
+                UserUserMapping.where( primeUserID: @toUser).update_all(sent: @secUserLikedData)
 
 
-        else
-
-            @presUserSentArr=(presUserData["sent"]).split(',')
-            @presUserSentArr=@presUserSentArr+@toUse
-            @presUserSent=@presUserSentArr.join(',')
-            UserUserMapping.where( primeUserID: @fromUser ).update_all( sent: @presUserSent )
-
-
-            @secUserRcv=secUserData["received"]
-            @secUserRcv=@secUserRcv+","+@fromUser
-            UserUserMapping.where( primeUserID: @toUser ).update_all( received: @secUserRcv )
-			end
-       elsif(@response == 'N')
+                @primUserBeingLikedArr=(@presUserData["received"]).split(',')
+                @primUserBeingLikedArr=@primUserBeingLikedArr-[@toUser]
+                @primUserBeingLiked=@primUserBeingLikedArr.join(',')
+                UserUserMapping.where( primeUserID: @fromUser).update_all(received: @primUserBeingLiked)
+            
+            end
         #user-user mapping
-				puts '@response == \'N\''
-       else
+        else
          puts 'Some Issue'
        end
 
