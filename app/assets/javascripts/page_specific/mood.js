f50e867a7474ac36c6310b1209d80407d59e714e
abbasinasb <a.abbasinasab@gmail.com>
2017-11-04 17:21:41 -0700

    created a structure for adding JS file for a specific page -- PLEASE READ my readme file under assets/js

---
+++ app/assets/javascripts/page_specific/mood.js
@@ -0,0 +1,15 @@
+console.log("sssss")
+
+function getRandomSize(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+var allImages = "";
+
+for (var i = 0; i < 25; i++) {
+  var width = getRandomSize(200, 400);
+  var height =  getRandomSize(200, 400);
+  allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
+}
+console.log("sssss2222ss")
+$('#photos').append(allImages);
